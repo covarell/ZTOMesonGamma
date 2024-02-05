7fdd0cadf6b35705d1b33e4c7bafa2a65c866b09
edoardo ferrando <edoardo.ferrando@edu.unito.it>
2024-02-05 13:13:46 +0100

    bkg fit

--- test/MassAnalysis/fit_bkg.py
+++ test/MassAnalysis/fit_bkg.py
@@ -85,16 +85,18 @@ parList = ROOT.RooArgSet(land_add_mean, land_add_sigma, gaus_mean, gaus_sigma)
 
 #Input file and tree ---------------------------------------------------------------
 if isPhiGammaAnalysis:
-    fileInput = ROOT.TFile("histos/latest_productions/CR_Phi_BDT_Sidebands.root")###########################
+    fileInput = ROOT.TFile("histos/latest_productions/CR_Phi_BDT_SidebandsNorm.root")###########################
 else :
-    fileInput = ROOT.TFile("histos/latest_productions/CR_Rho_BDT_Sidebands.root")###########################
+    fileInput = ROOT.TFile("histos/latest_productions/CR_Rho_BDT_SidebandsNorm.root")###########################
 fileInput.cd()
-tree = fileInput.Get("tree_output")
+#tree = fileInput.Get("tree_output")
 
 #prepare a rebinned TH1 for Z mass ---------------------------------------------------------------
 #xLowRange  = 75.
 #xHighRange = 105.
-
+h_mZ = fileInput.Get("h_ZMass")
+if isPhiGammaAnalysis:
+     h_mZ.Rebin(2)
 '''
 if isPhiGammaAnalysis: h_mZ = ROOT.TH1F("h_mZ","h_mZ", 150, 50., 200.)
 else: h_mZ =  ROOT.TH1F("h_mZ","h_mZ", 300, 50., 200.)
@@ -115,11 +117,20 @@ for jentry in xrange(nentries):
     h_mZ.Fill(tree.ZMass)
 
 '''
+
 #Retrieve observed_data from the tree, insert the variable also ---------------------------------------------------------------
-observed_data = ROOT.RooDataSet("observed_data","observed_data",ROOT.RooArgSet(mass),ROOT.RooFit.Import(tree))
-#observed_data = ROOT.RooDataHist("observed_data", "observed_data", ROOT.RooArgList(mass), h_mZ)############
+#observed_data = ROOT.RooDataSet("observed_data","observed_data",ROOT.RooArgSet(mass),ROOT.RooFit.Import(tree))
+observed_data = ROOT.RooDataHist("observed_data", "observed_data", ROOT.RooArgList(mass), h_mZ)############
 nEntries = observed_data.numEntries() 
 print "nEntries = ",nEntries
+
+if isPhiGammaAnalysis:
+    SR_input = ROOT.TFile("histos/latest_productions/SR_Phi_BDT_Signal.root")
+else :
+    SR_input = ROOT.TFile("histos/latest_productions/SR_Rho_BDT_Signal.root")
+
+h_mZ_SR = SR_input.Get("h_ZMass")
+True_data = ROOT.RooDataHist("observed_data", "observed_data", ROOT.RooArgList(mass), h_mZ_SR)
 
 
 #Do the fit ------------------------------------------------------------------------------------------------------------------------------
@@ -191,8 +202,8 @@ multipdf = ROOT.RooMultiPdf("multipdf_"+CHANNEL+"_bkg","All Pdfs",cat,mypdfs)
 
 #create Workspace ------------------------------------------------------------------------------------------------------------------------------
 norm   = nEntries 
-#norm1 = h_mZ.Integral()################
-print "************************************** n. events = ",nEntries#, "norm = ", norm1
+norm1 = h_mZ.Integral()################
+print "************************************** n. events = ",nEntries, "norm = ", norm1
 bkg_norm = ROOT.RooRealVar(multipdf.GetName()+ "_norm", multipdf.GetName()+ "_norm", norm, 0.5*norm, 2*norm)#########
 
 inputWS = ROOT.TFile("workspaces/workspace_"+CHANNEL+".root")  
@@ -200,7 +211,7 @@ inputWS.cd()
 workspace = inputWS.Get("workspace_"+CHANNEL+"")
 getattr(workspace,'import')(cat)
 getattr(workspace,'import')(multipdf)
-getattr(workspace,'import')(observed_data)
+getattr(workspace,'import')(True_data)
 getattr(workspace,'import')(bkg_norm)
 print "integral BKG :",bkg_norm.Print()
 
